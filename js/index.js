9ddeea83012f33d17c898debf8071001a48ac6e0
Evgenii Dementev <63846200+ClickDaHouseCat@users.noreply.github.com>
2024-01-24 03:49:30 +0300

    feat: add interactive and blur on cards

--- js/index.js
+++ js/index.js
@@ -1,49 +1,52 @@
-// Бургер меню
+// // Бургер меню
 
-const burger = document.querySelector(".burgerMenu")
-const burgerBtn = document.querySelector(".burgerBtn")
-const burgerBtnClose = document.querySelector(".burgerBtnClose")
-const burgLi = document.querySelectorAll(".burgLi")
+// const burger = document.querySelector(".burgerMenu")
+// const burgerBtn = document.querySelector(".burgerBtn")
+// const burgerBtnClose = document.querySelector(".burgerBtnClose")
+// const burgLi = document.querySelectorAll(".burgLi")
 
-    burgerBtn.addEventListener('click', ()=> {
-        burger.classList.toggle('open')
-    })
-    // Выбираем каждый элемент с классом burgLi
-    document.querySelectorAll('.burgLi').forEach((elem) =>
-    elem.addEventListener('click', () => {
-        burger.classList.toggle('open')
-    }))
+//     burgerBtn.addEventListener('click', ()=> {
+//         burger.classList.toggle('open')
+//     })
+//     // Выбираем каждый элемент с классом burgLi
+//     document.querySelectorAll('.burgLi').forEach((elem) =>
+//     elem.addEventListener('click', () => {
+//         burger.classList.toggle('open')
+//     }))
 
-    // Закрываем меню по нажатию в любое место
-    document.querySelectorAll('.main'),forEach((elem) =>
-    elem.addEventListener('click', ()=> {
-        burger.classList.remove('open')
-    }))
+//     // Закрываем меню по нажатию в любое место
+//     document.querySelectorAll('.main'),forEach((elem) =>
+//     elem.addEventListener('click', ()=> {
+//         burger.classList.remove('open')
+//     }))
 
-    burgerBtnClose.addEventListener('click', ()=> {
-        burger.classList.toggle('open')
-    })
+//     burgerBtnClose.addEventListener('click', ()=> {
+//         burger.classList.toggle('open')
+//     })
 
-// Карточки сервисов и проектов
+const gardensBtn = document.querySelector(".gardensBtn");
+const lawnBtn = document.querySelector(".lawnBtn");
+const plantingBtn = document.querySelector(".plantingBtn");
 
-const gardensBtn = document.querySelector(".gardensBtn")
-const lawnBtn = document.querySelector(".lawnBtn")
-const plantingBtn = document.querySelector(".plantingBtn")
-const gardensCrd = document.querySelector(".gardensCrd")
-const lawnCrd = document.querySelector(".lawnCrd")
-const plantingCrd = document.querySelector(".plantingCrd")
+const gardenCrd = document.querySelectorAll(".gardenCrd");
+const lawnCrd = document.querySelectorAll(".lawnCrd");
+const plantingCrd = document.querySelectorAll(".plantingCrd");
 
 gardensBtn.addEventListener('click', () => {
-    lawnCrd.classList.toggle('blur');
-    plantingCrd.classList.toggle('blur');
-})
+    lawnCrd.forEach(card => card.classList.add('blur'));
+    plantingCrd.forEach(card => card.classList.add('blur'));
+    gardenCrd.forEach(card => card.classList.remove('blur'));
+});
 
 lawnBtn.addEventListener('click', () => {
-    gardenCrd.classList.toggle('blur');
-    plantingCrd.classList.toggle('blur');
-})
+    plantingCrd.forEach(card => card.classList.add('blur'));
+    gardenCrd.forEach(card => card.classList.add('blur'));
+    lawnCrd.forEach(card => card.classList.remove('blur'));
+});
 
 plantingBtn.addEventListener('click', () => {
-    lawnCrd.classList.toggle('blur');
-    gardenCrd.classList.toggle('blur');
-})
+    gardenCrd.forEach(card => card.classList.add('blur'));
+    lawnCrd.forEach(card => card.classList.add('blur'));
+    plantingCrd.forEach(card => card.classList.remove('blur'));
+});
+
